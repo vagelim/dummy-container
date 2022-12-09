3543c004f8278b52d19929b49aae22ceee5d714f
vagelim <gtfo@evangelosm.com>
2022-12-09 12:47:34 -0500

    add logging

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-from subprocess import check_output as sub
+from subprocess import check_output as run
+import logging
+from flask import Flask, request
 
-from flask import Flask
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
@@ -10,10 +12,10 @@ command = ["dig","+short","ch","txt","whoami.cloudflare","@1.1.1.1"]
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def ip(path):
-    output= sub(command).decode('ascii')
+    output= run(command,universal_newlines=True)
+    app.logger.info(f"ip from request: {request.remote_addr}")
+    app.logger.info(f"public ip: {output}")
     return output
 
 if __name__ == '__main__':
-    output= sub(command).decode('ascii')
-    print(output)
     app.run(host='0.0.0.0', port=4000)
